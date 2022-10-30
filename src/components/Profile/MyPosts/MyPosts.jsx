a56d275a98e314a8bc21d30837653629a3a1053c
Viktar <viktar13@hotmail.com>
2022-10-30 12:49:05 +0400

    15 craching Profile folder

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,19 @@
+import s from "./MyPosts.module.css";
+import Post from "./Post/Post";
+
+const MyPosts = () => {
+  return (
+    <div>
+      My posts
+      <div>New post</div>
+      <div className={s.posts}>
+        <Post />
+        <Post />  
+        <Post />
+        <Post />       
+      </div>
+    </div>
+  );
+};
+
+export default MyPosts;
---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,11 @@
+import s from "./Post.module.css";
+
+const Post = () => {
+  return (
+  <div className={s.item}>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7wIM1l36lND_jsEwki3UCnRpcTzs-AfPv-A&usqp=CAU"></img>
+    post 1</div>
+  );
+};
+
+export default Post;
